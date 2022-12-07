2783874250dee20427bef21d025c7180f8d87049
ming.ming <1210919685@qq.com>
2022-12-07 09:53:28 +0800

    增加合约注释说明

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -49,16 +49,18 @@ contract NFTMarketplace is ERC721URIStorage {
     }
 
     /* Mints a token and lists it in the marketplace */
+    // tokenURI 也就是资源在ipfs的路径，资源本身就是代币(Token),tokenURI就是nft的uri  
     function createToken(string memory tokenURI, uint256 price) public payable returns (uint) {
       _tokenIds.increment();
       uint256 newTokenId = _tokenIds.current();
-
-      _mint(msg.sender, newTokenId);
-      _setTokenURI(newTokenId, tokenURI);
+                                     //  _owners[newTokenId] = msg.sender
+      _mint(msg.sender, newTokenId); // Mints `newTokenId` and transfers it to `msg.sender`.  // newTokenId 全局唯一的，并在状态变量里记录  
+      _setTokenURI(newTokenId, tokenURI); // mapping(uint256 => string) private _tokenURIs; 存储使用  
       createMarketItem(newTokenId, price);
       return newTokenId;
     }
 
+    /*  通过tokenId记录MarketItem信息，并且  */
     function createMarketItem(
       uint256 tokenId,
       uint256 price
@@ -73,8 +75,8 @@ contract NFTMarketplace is ERC721URIStorage {
         price,
         false
       );
-
-      _transfer(msg.sender, address(this), tokenId);
+                                                     // msg.sender指的是调用合约的地址 address(this)正在调用的合约实例的地址
+      _transfer(msg.sender, address(this), tokenId); // Transfers `tokenId` from `msg.sender` to `address(this)`.  _owners[tokenId] = address(this);
       emit MarketItemCreated(
         tokenId,
         msg.sender,
@@ -103,15 +105,15 @@ contract NFTMarketplace is ERC721URIStorage {
       uint256 tokenId
       ) public payable {
       uint price = idToMarketItem[tokenId].price;
-      address seller = idToMarketItem[tokenId].seller;
+      address seller = idToMarketItem[tokenId].seller;    // 之前铸造的人  
       require(msg.value == price, "Please submit the asking price in order to complete the purchase");
       idToMarketItem[tokenId].owner = payable(msg.sender);
       idToMarketItem[tokenId].sold = true;
-      idToMarketItem[tokenId].seller = payable(address(0));
+      idToMarketItem[tokenId].seller = payable(address(0)); // 置空了  
       _itemsSold.increment();
       _transfer(address(this), msg.sender, tokenId);
-      payable(owner).transfer(listingPrice);
-      payable(seller).transfer(msg.value);
+      payable(owner).transfer(listingPrice);  // owner = payable(msg.sender);  owner 之前部署合约的人  
+      payable(seller).transfer(msg.value);    // <address>.transfer(uint256 amount)  向address发送amount数量的Wei , seller 以相应的价格卖出  
     }
 
     /* Returns all unsold market items */
